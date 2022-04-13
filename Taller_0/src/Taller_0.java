d1f70a5ea46e0b653b671047cb88e217b2fb2690
CristianMoyano13 <89607609+CristianMoyano13@users.noreply.github.com>
2022-04-13 01:27:45 -0400

    Add files via upload

--- Taller_0/src/Taller_0.java
+++ Taller_0/src/Taller_0.java
@@ -3,9 +3,30 @@ import java.io.*;
 public class Taller_0 {
 
 	public static void main(String[] args) {
+		//Listas archivo "Jugadores.txt"
 		String [] nombres = new String [999];
 		String [] contraseñas = new String [999];
-
+		int [] vida = new int [999];
+		int [] ataque = new int [999];
+		int [] defensa = new int [999];
+		int [] velocidad = new int [999];
+		int [] hechizos = new int [999];
+		int [] xp = new int [999];
+		
+		//Listas archivo "Hechizos.txt"
+		String [] nomHechizo = new String[999];
+		int [] poderAtaque = new int [999];
+		
+		//Listas archivo "HechizosJugadores.txt"
+		String [] nomJugador = new String[999];
+		String [] nomHechizoJ = new String[999];
+		
+		//Listas archivo "Enemigos.txt"
+		String [] nombreE = new String[999];
+		int [] vidaE = new int [999];
+		int [] ataqueE = new int [999];
+		String [] tipoE = new String[999];
+		int [] velocidadE = new int [999];
 	}
 	public static int carga_J() throws NumberFormatException, IOException{
 		String archivo= "Jugadores.txt";
@@ -26,20 +47,37 @@ public class Taller_0 {
         reader.close();
         return cant;
 	}
-	public static void carga_H()throws NumberFormatException, IOException {
+	public static int carga_H()throws NumberFormatException, IOException {
 		String archivo= "Hechizos.txt";
         Scanner reader = new Scanner(archivo);
+        int cant = 0;
         while(reader.hasNextLine()){	
             String [] partes = archivo.split(", ");
-            String nombre = partes[0];
-            String nomHechizo = partes[1];
-            int  poderAtaque = Integer.parseInt(partes[2]);
+            String nomHechizo = partes[0];
+            int  poderAtaque = Integer.parseInt(partes[1]);
+            cant++;
         }
         reader.close();
+        return cant;
 	}
-	public static void carga_E()throws NumberFormatException, IOException {
+	public static int carga_HechizosJugadores()throws NumberFormatException, IOException {
+		String archivo= "HechizosJugadores.txt";
+        Scanner reader = new Scanner(archivo);
+        int cant = 0;
+        while(reader.hasNextLine()){	
+            String [] partes = archivo.split(", ");
+            String nomJugador = partes[0];
+            String nomHechizo = partes[1];
+            cant++;
+        }
+        reader.close();
+        return cant;
+	}
+	
+	public static int carga_E()throws NumberFormatException, IOException {
 		String archivo= "Enemigos.txt";
         Scanner reader = new Scanner(archivo);
+        int cant = 0;
         while(reader.hasNextLine()){	
             String [] partes = archivo.split(", ");
             String nombreE = partes[0];
@@ -47,10 +85,11 @@ public class Taller_0 {
             int  ataqueE = Integer.parseInt(partes[3]);
             String tipo = partes[4];
             int  velocidadE = Integer.parseInt(partes[5]);
+            cant++;
         }
         reader.close();
+        return cant;
 	}
-	
 	public static void inicioSesion(String contraseñas[], String nombres[],int cantidadUsuarios) {
 		Scanner scan = new Scanner(System.in);
 		boolean llave = true;
@@ -106,10 +145,8 @@ public class Taller_0 {
 			}
 		}
 	}
-	
 	private static void agregar_Jugador() {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub	
 	}
 	private static void menu_cliente() {
 		Scanner scan = new Scanner(System.in);
@@ -121,6 +158,7 @@ public class Taller_0 {
 		System.out.println("Ingrese el numero del menú al que quiera ingresar: ");
 		String opcion = scan.nextLine();
 		if (opcion.equals("1")){
+			
 		}
 		else if(opcion.equals("2")){
 		}
@@ -133,7 +171,6 @@ public class Taller_0 {
 		else {
 			System.out.println("Opcion Incorrecta");
 		}
-		
 	}
 	public static int index(String[] list, String argument, int cant) {
         for (int i = 0; i < cant; i++) {
@@ -143,7 +180,6 @@ public class Taller_0 {
         }
         return -1;
 	}
-	
 	private static void menu_admin() {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("1) Eliminar Jugador");
@@ -153,18 +189,56 @@ public class Taller_0 {
 		System.out.println("Ingrese el numero del menú al que quiera ingresar: ");
 		String opcion = scan.nextLine();
 		if (opcion.equals("1")){
+			//eliminarJugador();
 		}
 		else if(opcion.equals("2")){
+			agregarEnemigo();
 		}
 		else if(opcion.equals("3")){
+			agregarHechizo();
 		}
 		else if(opcion.equals("4")){
+			verEstadisticas();
 		}
 		else {
 			System.out.println("Opcion Incorrecta");
-			
 		}
-		
 	}
-
+	private static void eliminarJugador(int cantidadUsuarios, String[] nombres, int contraseñas[], int vida[], int ataque[], int defensa[], int velocidad[], int hechizos[], int xp[], String []nomJugador, String []nomHechizoJ){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Eliminar Jugador");
+		System.out.println("-------------------");
+		System.out.println("Escriba el Nombre del jugador a eliminar: ");
+		String eliminado = scan.nextLine();
+		int indexUsuario = index(nombres, eliminado, cantidadUsuarios);
+		for (int i = 0; i < nombres.length; i++) {
+			if (eliminado.equals(nombres[i]) && indexUsuario == i) {
+				System.out.println("Se ha encontrado al jugador");
+				System.out.println("Eliminando...");
+				nombres = null;
+	            contraseñas = null;
+	            vida = null;
+	            ataque = null;
+	            defensa = null;
+	            velocidad = null;
+	            hechizos = null;
+	            xp = null;
+	            nomJugador = null;
+	            nomHechizoJ = null;
+			}
+			else {
+				System.out.println("No se ha encontrado al jugador");
+			}
+		}
+	}
+	private static void verEstadisticas() {
+		// TODO Auto-generated method stub	
+	}
+	private static void agregarHechizo() {
+		// TODO Auto-generated method stub	
+	}
+	private static void agregarEnemigo() {
+		// TODO Auto-generated method stub	
+	}
+	
 }
